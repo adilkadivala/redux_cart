12e670d6149733ba748b01b8f5b8c23178ab9a0e
k-adi <adilkadivala560@gmail.com>
2024-06-25 15:12:26 +0530

    implementation

---
+++ server/controller/product.js
@@ -0,0 +1,1 @@
+const connectDB = require("../database/connect");
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const database = require("./server/connect");
+const database = require("./database/connect");
 
 // middleware
 app.use(express.json());
