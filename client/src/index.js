2f74455fd76a7024931b4c248b7014e3d5532524
k-adi <adilkadivala560@gmail.com>
2024-06-26 12:47:41 +0530

    some implementation

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ItemStorage } from "./store/ProductData";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ItemStorage>
+      <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </ItemStorage>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
---
+++ client/src/store/ProductData.js
@@ -0,0 +1,31 @@
+import { useContext, createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+// Making context
+export const ItemData = createContext();
+
+// Setting context in a function
+export const useItemData = () => useContext(ItemData);
+
+export const ItemStorage = ({ children }) => {
+  const [item, setItem] = useState([]);
+  console.log(item);
+
+  // Getting item
+  const getItem = async () => {
+    try {
+      const response = await axios.get("http://localhost:5236/getproduct");
+      setItem(response.data);
+    } catch (error) {
+      console.error("Error fetching item list:", error);
+    }
+  };
+
+  useEffect(() => {
+    getItem();
+  }, []);
+
+  return (
+    <ItemData.Provider value={{ item, setItem }}>{children}</ItemData.Provider>
+  );
+};
